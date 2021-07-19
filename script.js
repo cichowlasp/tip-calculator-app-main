69ee6a23724c2a679ae2e9038feb4ad03b9fa3c0
cichowlasp <cichowlas.p@gmail.com>
2021-07-19 16:30:45 +0200

    added logic

--- script.js
+++ script.js
@@ -3,11 +3,46 @@ let total = document.querySelector('div[total]');
 let tipContainer = document.querySelectorAll('div[class=option]');
 let peopleInput = document.querySelector('input[people]');
 let tipAmount = document.querySelector('div[tip]');
-let selectedTip;
+let customInput = document.querySelector('[placeholder="Custom"]');
+let selectedTip = 0;
+let resetButton = document.querySelector('button[reset]');
+
+const defaultValue = `$0.00`;
+const percentsValues = {
+	'5%': 0.05,
+	'10%': 0.1,
+	'15%': 0.15,
+	'25%': 0.25,
+	'50%': 0.5,
+};
+
+const errorMessages = {
+	peopleInput: 'Must be bigger than zero',
+	billInput: 'Must be bigger than zero',
+};
+
+const setInputError = (inputName, paragraph, text) => {
+	let error = document.querySelector(paragraph);
+	let input = document.querySelector(inputName);
+	error.textContent = text;
+	input.classList.add('error');
+};
+
+const clearInputError = (inputName, paragraph) => {
+	let error = document.querySelector(paragraph);
+	let input = document.querySelector(inputName);
+	error.textContent = '';
+	input.classList.remove('error');
+};
 
 billInput.addEventListener('input', () => {
+	clearInputError('input[bill]', 'p[bill-error]');
+	calculateTip();
 	calculateTotal();
-	if (billInput.value === '') total.textContent = `$0.00`;
+	if (billInput.value <= 0) {
+		total.textContent = defaultValue;
+		setInputError('input[bill]', 'p[bill-error]', errorMessages.billInput);
+	}
 });
 
 const removeActiveClass = () => {
@@ -17,6 +52,13 @@ const removeActiveClass = () => {
 		}
 	});
 };
+
+customInput.addEventListener('input', () => {
+	removeActiveClass();
+	selectedTip = customInput.value / 100;
+	calculateTip();
+	calculateTotal();
+});
 
 tipContainer.forEach((element) => {
 	element.addEventListener('click', () => {
@@ -28,17 +70,12 @@ tipContainer.forEach((element) => {
 	});
 });
 
-const percentsValues = {
-	'5%': 0.05,
-	'10%': 0.1,
-	'15%': 0.15,
-	'25%': 0.25,
-	'50%': 0.5,
-};
-
 const calculateTotal = () => {
-	const percent = percentsValues[selectedTip.textContent];
-	const price = parseInt(billInput.value);
+	const percent =
+		percentsValues[selectedTip.textContent] !== undefined
+			? percentsValues[selectedTip.textContent]
+			: selectedTip;
+	const price = parseFloat(billInput.value) ? parseFloat(billInput.value) : 0;
 	const peopleNumber = parseInt(peopleInput.value);
 	total.textContent = `$${((price * percent + price) / peopleNumber).toFixed(
 		2
@@ -48,15 +85,43 @@ const calculateTotal = () => {
 };
 
 const calculateTip = () => {
-	const percent = percentsValues[selectedTip.textContent];
-	const price = parseInt(billInput.value);
+	const percent =
+		percentsValues[selectedTip.textContent] !== undefined
+			? percentsValues[selectedTip.textContent]
+			: selectedTip;
+	const price = billInput.value;
 	const peopleNumber = parseInt(peopleInput.value);
 	tipAmount.textContent = `$${((percent * price) / peopleNumber).toFixed(2)}`;
-	if (selectedTip.textContent === '' || peopleInput.value === '')
+	console.log(price);
+	if (selectedTip.textContent === '' || peopleInput.value === '') {
 		tipAmount.textContent = `$${(percent * billInput.value).toFixed(2)}`;
+	}
 };
 
 peopleInput.addEventListener('input', () => {
-	calculateTip();
-	calculateTotal();
+	if (peopleInput.value >= 1) {
+		clearInputError('input[people]', 'p[people-error]');
+		peopleInput.classList.remove('error');
+		calculateTip();
+		calculateTotal();
+	} else {
+		setInputError(
+			'input[people]',
+			'p[people-error]',
+			errorMessages.peopleInput
+		);
+		total.textContent = defaultValue;
+		tipAmount.textContent = defaultValue;
+	}
+});
+
+resetButton.addEventListener('click', () => {
+	removeActiveClass();
+	clearInputError('input[people]', 'p[people-error]');
+	clearInputError('input[bill]', 'p[bill-error]');
+	billInput.value = undefined;
+	customInput.value = undefined;
+	peopleInput.value = undefined;
+	total.textContent = defaultValue;
+	tipAmount.textContent = defaultValue;
 });
